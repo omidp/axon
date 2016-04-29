a7469715df42fa8f22637dc01a383b5da9674fe7
Omid Pourhadi <omidpourhadi@gmail.com>
2016-04-29 15:06:14 +0430

    Update AxonInvocationHandler.java
    exclude proxies for serialization

--- axon-hibernate/src/main/java/org/omidbiz/core/axon/hibernate/AxonInvocationHandler.java
+++ axon-hibernate/src/main/java/org/omidbiz/core/axon/hibernate/AxonInvocationHandler.java
@@ -3,10 +3,17 @@ package org.omidbiz.core.axon.hibernate;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 import org.hibernate.Hibernate;
 import org.hibernate.Session;
+import org.hibernate.engine.SessionImplementor;
+import org.hibernate.impl.SessionImpl;
 import org.hibernate.proxy.HibernateProxy;
+import org.hibernate.proxy.LazyInitializer;
+import org.hibernate.proxy.pojo.javassist.JavassistLazyInitializer;
 import org.omidbiz.core.axon.AxonBeanHelper;
 import org.omidbiz.core.axon.Property;
 import org.omidbiz.core.axon.filters.RecursionControlFilter;
@@ -20,6 +27,16 @@ import org.omidbiz.core.axon.filters.RecursionControlFilter;
  */
 public class AxonInvocationHandler implements InvocationHandler
 {
+
+    static List<Class<?>> excludedList = Collections.synchronizedList(new ArrayList<Class<?>>());
+
+    static
+    {
+        excludedList.add(LazyInitializer.class);
+        excludedList.add(Object.class);
+        excludedList.add(Class.class);
+        excludedList.add(SessionImplementor.class);
+    }
 
     private Object delegate;
     private Session session;
@@ -48,6 +65,8 @@ public class AxonInvocationHandler implements InvocationHandler
                     Property property = (Property) args[2];
                     Object value = args[3];
                     Object invoke = null;
+                    if (property == null)
+                        return method.invoke(delegate, args[0], targetEntity, property, value);
                     if (AxonBeanHelper.isPrimitiveOrWrapper(property.getGetter().getReturnType()) == false)
                     {
                         // no way to prevent closing session, we have to hit db
@@ -59,7 +78,19 @@ public class AxonInvocationHandler implements InvocationHandler
                         // initializeAndUnproxy(targetEntity);
                         // session.refresh(targetEntity);
                         // session.close();
-                        return Hibernate.isPropertyInitialized(targetEntity, property.getName());
+                        if (Hibernate.isPropertyInitialized(targetEntity, property.getName()))
+                        {
+                            if (excludedList.contains(property.getType()))
+                                return true;
+                            if (property.getType().getName().equals("javassist.util.proxy.MethodHandler"))
+                                return true;
+                            return false;
+                        }
+                        else
+                        {
+                            // exclude element
+                            return true;
+                        }
                     }
                     invoke = method.invoke(delegate, args[0], targetEntity, property, value);
                     return invoke;
